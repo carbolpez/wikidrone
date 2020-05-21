ccfaf149196ac6e5fd0a96c8f1a3e7715269e72e
Carlos Bolaños <carbolpez@gmail.com>
2020-05-21 15:09:11 +0200

    error navegación

--- src/components/detail/DetailContainer.vue
+++ src/components/detail/DetailContainer.vue
@@ -9,7 +9,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active"><a href="/register">Tracks</a></li>
+              <li class="breadcrumb-item active"><a href="/tracks">Tracks</a></li>
             </ol>
           </div>
         </div>
